daf3ebd29f555e3f457768bfd0aa1bdc21cb91d7
chuggajohn <jab550@mun.ca>
2019-03-05 15:21:40 -0330

    got bounding boxes on humans working limbs next

--- src/HumanPose.py
+++ src/HumanPose.py
@@ -1,23 +1,27 @@
 import cv2
 import numpy as np
+from imutils.object_detection import non_max_suppression
 
 def readVideo():
-    cam = cv2.VideoCapture(0)
-    ret, frame = cam.read()
-    w = cam.get(3)
-    h = cam.get(4)
-    print(w,h)
+    #cam = cv2.VideoCapture(0)
+    #ret, frame = cam.read()
+    frame = cv2.imread("..\images\man.jpg")
+    #w = cam.get(3)
+    #h = cam.get(4)
+    #print(w,h)
     k = cv2.waitKey(1)
     hog = cv2.HOGDescriptor()
     hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector()) 
        
     while (k%256 != 27):
         (rects, weights) = hog.detectMultiScale(frame, winStride=(4, 4),padding=(8, 8), scale=1.05)
-        for (x, y, w, h) in rects:
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+        pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+        for (xA, yA, xB, yB) in pick:
+            cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
         k = cv2.waitKey(1)
         cv2.imshow('test' , np.array(frame, dtype = np.uint8 ))
-        ret, frame = cam.read()
+        #ret, frame = cam.read()
 
 def histogramOfGradients(img):
     gx = cv2.Sobel(img, cv2.CV_32F, 1, 0, ksize=1)
