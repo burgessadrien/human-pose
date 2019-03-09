cd1c7511693dc23412cc7b452717c5f595d2ae91
Adrien Burgess <a.burke.burgess@gmail.com>
2019-03-09 17:34:30 -0330

    able to detect face

--- src/HumanPose.py
+++ src/HumanPose.py
@@ -13,41 +13,41 @@ def readVideo():
     cam.set(3,320)
     cam.set(4,240)
     k = cv2.waitKey(1)
-    kernel = np.ones((8,2), np.uint8)
+    kernel = np.ones((5,5), np.uint8)
     hog = cv2.HOGDescriptor()
     hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-    #ret, frame = cam.read()
-    frame = cv2.imread("..\images\man.jpg")
-    #w = cam.get(3)
-    #h = cam.get(4)
-    #print(w,h)
-    width = frame.shape[0]
-    height = frame.shape[1]
-    canvas = np.zeros((width, height, 3), np.uint8)
-    
-
-    (rects, weights) = hog.detectMultiScale(frame, winStride=(4, 4),padding=(8, 8), scale=1.05)
-    rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
-    pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
-    for (xA, yA, xB, yB) in pick:
-        #cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
-        human = frame[yA:yB, xA:xB]
-        threshHuman = cv2.Canny(human,CANNY_THRESH_1,CANNY_THRESH_2)        
-        contours,high = cv2.findContours(threshHuman, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-        c  = sorted(contours, key=cv2.contourArea, reverse=True)
-        cv2.drawContours(canvas, c, 1, (0,255,0), 1)
+    face_cascade = cv2.CascadeClassifier('venv/lib/python3.7/site-packages/cv2/data/haarcascade_frontalface_alt.xml')
     while (k%256 != 27):
-        cv2.imshow('image', np.array(frame, dtype = np.uint8))
-        cv2.imshow('canvas', np.array(threshHuman, dtype = np.uint8))
-        cv2.imshow('human' , np.array(human, dtype = np.uint8 ))
-        cv2.imshow('test' , np.array(canvas, dtype = np.uint8 ))
-        k = cv2.waitKey(1)
-
-
-        
-        
         #ret, frame = cam.read()
+        frame = cv2.imread("images/stockperson.jpg")
+        #w = cam.get(3)
+        #h = cam.get(4)
+        #print(w,h)
+        width = frame.shape[0]
+        height = frame.shape[1]
+        canvas = np.zeros((width, height, 3), np.uint8)
+        #frame = cv2.medianBlur(frame, 5)
+        (rects, weights) = hog.detectMultiScale(frame, winStride=(4, 4),padding=(8, 8), scale=1.05)
+        rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+        pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+        if len(pick) > 0:
+            (xA, yA, xB, yB) = pick[0]
+            #cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
+            human = frame[yA:yB, xA:xB]
+            grayHuman = cv2.cvtColor(human, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(grayHuman, 1.3, 5)
+            for (x,y,w,h) in faces:
+                cv2.rectangle(human,(x,y),(x+w,y+h),(255,255,0),2)
+            threshHuman = cv2.Canny(human,CANNY_THRESH_1,CANNY_THRESH_2)
+            contours,high = cv2.findContours(threshHuman, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+            c  = sorted(contours, key=cv2.contourArea, reverse=True)
+            cv2.drawContours(canvas, c, -1, (255,255,255), -1)
+            cv2.imshow('human' , np.array(threshHuman, dtype = np.uint8 ))
 
+        cv2.imshow('image', np.array(frame, dtype = np.uint8))
+        cv2.imshow('test' , np.array(canvas, dtype = np.uint8 ))
+            #k = cv2.waitKey(1)
+        k = cv2.waitKey(1)
 
 if __name__ == "__main__":
     readVideo()
