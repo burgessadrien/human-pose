5b06fc9e9c3724f58aabee0059060f2d002b4b44
Adrien Burgess <a.burke.burgess@gmail.com>
2019-03-09 20:31:07 -0330

    shoulder detection and new images

--- src/HumanPose.py
+++ src/HumanPose.py
@@ -1,8 +1,14 @@
 import cv2
 import numpy as np
 from imutils.object_detection import non_max_suppression
+
 CANNY_THRESH_1 =10
 CANNY_THRESH_2 = 200
+
+face_cascade = cv2.CascadeClassifier('venv/lib/python3.7/site-packages/cv2/data/haarcascade_frontalface_alt.xml')
+upper_body_cascade = cv2.CascadeClassifier('venv/lib/python3.7/site-packages/cv2/data/haarcascade_upperbody.xml')
+lower_body_cascade = cv2.CascadeClassifier('venv/lib/python3.7/site-packages/cv2/data/haarcascade_lowerbody.xml')
+
 def readVideo():
     try:
         cam = cv2.VideoCapture(0)
@@ -16,7 +22,7 @@ def readVideo():
     kernel = np.ones((5,5), np.uint8)
     hog = cv2.HOGDescriptor()
     hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
-    face_cascade = cv2.CascadeClassifier('venv/lib/python3.7/site-packages/cv2/data/haarcascade_frontalface_alt.xml')
+    
     while (k%256 != 27):
         #ret, frame = cam.read()
         frame = cv2.imread("images/stockperson.jpg")
@@ -26,7 +32,7 @@ def readVideo():
         width = frame.shape[0]
         height = frame.shape[1]
         canvas = np.zeros((width, height, 3), np.uint8)
-        #frame = cv2.medianBlur(frame, 5)
+        frame = cv2.medianBlur(frame, 5)
         (rects, weights) = hog.detectMultiScale(frame, winStride=(4, 4),padding=(8, 8), scale=1.05)
         rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
         pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
@@ -35,9 +41,15 @@ def readVideo():
             #cv2.rectangle(frame, (xA, yA), (xB, yB), (0, 255, 0), 2)
             human = frame[yA:yB, xA:xB]
             grayHuman = cv2.cvtColor(human, cv2.COLOR_BGR2GRAY)
-            faces = face_cascade.detectMultiScale(grayHuman, 1.3, 5)
-            for (x,y,w,h) in faces:
-                cv2.rectangle(human,(x,y),(x+w,y+h),(255,255,0),2)
+            [x,y,w,h] = face_cascade.detectMultiScale(grayHuman, 1.3, 5)[0]
+            cv2.rectangle(human,(x,y),(x+w,y+h),(255,255,0),2)
+            [a,b,c,d] = upper_body_cascade.detectMultiScale(grayHuman, scaleFactor = 1.1, minNeighbors = 5, minSize = (30,30),flags = cv2.CASCADE_SCALE_IMAGE)[0]
+            b = b + y
+            d = d - y
+            cv2.rectangle(human,(a,b),(a+c,b+d),(255,0,0),2)
+            #[f,g,h,i] = lower_body_cascade.detectMultiScale(grayHuman, scaleFactor = 1.3, minNeighbors = 5, minSize = (30,30),flags = cv2.CASCADE_SCALE_IMAGE)[0]
+            #cv2.rectangle(human,(f,g),(f+h,g+i),(255,0,0),2)
+
             threshHuman = cv2.Canny(human,CANNY_THRESH_1,CANNY_THRESH_2)
             contours,high = cv2.findContours(threshHuman, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
             c  = sorted(contours, key=cv2.contourArea, reverse=True)
